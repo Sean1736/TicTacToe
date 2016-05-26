3ddd1c55abe8cd7d67da9c02eb8d73a0e29ec0a0
Sean1736 <milkwood72@gmail.com>
2016-05-26 14:19:42 -0500

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Cindy Barnsdale on 5/26/16.
+//  Copyright © 2016 Salar Kohnechi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
